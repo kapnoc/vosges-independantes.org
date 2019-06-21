34d2aa2adbd7517c02472ac018162e159adcb7c8
Tanguy Gérôme <me@kapno.cc>
2019-06-21 13:59:00 +0200

    Initial rust design

---
+++ src/dico/mod.rs
@@ -0,0 +1,36 @@
+use std::vec::Vec;
+
+use rusqlite;
+use rocket::http::RawStr;
+use rocket_contrib::templates::Template;
+use serde_json::json;
+
+#[get("/dico")]
+pub fn dico() -> Template {
+    let ctx = json!({
+        "title": "Dico"
+    });
+    Template::render("dico", &ctx)
+}
+
+#[get("/dico?<qweredje>")]
+pub fn dico_res(conn: crate::DicoDbConn, qweredje: &RawStr) -> Template {
+    let fr = qweredje.as_str().to_string();
+    let safe_qweredje = fr.replace(";", "").replace("\"", "");
+    let mut stmt  = conn
+        .prepare("SELECT fr, vo FROM defs WHERE fr LIKE \"%?1%\" ORDER BY id ASC"
+                 .to_string().replace("?1", safe_qweredje.as_str()).as_str()).unwrap();
+    let defs = stmt.query_map(&[], |row| {
+        let fr = row.get::<usize, String>(0);
+        let vo = row.get::<usize, String>(1);
+        Ok::<(String, String), rusqlite::Error>((fr, vo))
+    }).unwrap().into_iter()
+        .map(|x| x.unwrap().unwrap())
+        .collect::<Vec<(String, String)>>();
+    let ctx = json!({
+        "title": "Dico",
+        "fr": &fr,
+        "defs": defs
+    });
+    Template::render("dico", &ctx)
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+use rocket_contrib::templates::Template;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::databases::rusqlite;
+use serde_json::json;
+
+mod dico;
+use crate::dico::static_rocket_route_info_for_dico;
+use crate::dico::static_rocket_route_info_for_dico_res;
+
+#[database("sqlite_dico")]
+pub struct DicoDbConn(rusqlite::Connection);
+
+#[get("/")]
+fn index() -> Template {
+    let ctx = json!({
+        "title": "Accueil"
+    });
+    Template::render("index", &ctx)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![index, dico, dico_res])
+        .mount("/static", StaticFiles::from("./static"))
+        .attach(Template::fairing())
+        .attach(DicoDbConn::fairing())
+        .launch();
+}
