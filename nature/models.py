6cc58c3835c4001c25329a3c1f1d1eefcd79e0e4
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-11-05 20:33:37 +0100

    added NaturePage.body_vo

--- nature/models.py
+++ nature/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from martor.models import MartorField
 from django_kapnoc.models import MarkdownPage
 
 
 class NaturePage(MarkdownPage):
     description = models.TextField()
+    body_vo = MartorField()
 
     class Meta:
         app_label = 'nature'
